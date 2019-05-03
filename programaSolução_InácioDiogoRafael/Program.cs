ef37b1af9198e9694089f73467067f4d547ce164
Inácio Amerio <n.ito10@hotmail.com>
2019-05-03 12:13:37 +0100

    Adicionar arquivos de projeto.

---
+++ programaSolução_InácioDiogoRafael/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programaSolução_InácioDiogoRafael
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
