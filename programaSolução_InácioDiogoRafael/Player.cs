e207dac625faebc4360d1dc1ebcf9a14c3c26674
diogo-h <diogo.m.henriques98@gmail.com>
2019-05-04 22:25:03 +0100

    Alterar classe Player

--- programaSolução_InácioDiogoRafael/Player.cs
+++ programaSolução_InácioDiogoRafael/Player.cs
@@ -10,11 +10,15 @@ namespace programaSolução_InácioDiogoRafael
     /// </summary>
     class Player
     {
-        // variaveis
-
-        // lista do numero de fantasmas para o que o jogador tem na mao
-
-        // lista do numero de fantasmas para o que o jogador tem na dungeon
-
+        /// <summary>
+        /// Listas para o número de fantasmas que o jogador possui e para o 
+        /// número de jogadores que está na dungeon. Usamos uma lista em vez de
+        /// um array pois é possiver alterar o conteúdos da mesma 
+        /// dinamicamente.
+        /// </summary>
+        // Lista do número de fantasmas que o jogador possui, começando com 9
+        List<string> playerGhosts = new List<string>(9);
+        // Lista do número de fantasmas que estão na dungeon começando com 0
+        List<string> dungeonGhosts = new List<string>(0);
     }
 }
