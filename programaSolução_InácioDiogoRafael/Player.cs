b3c0bcb15a7af95b139fdebf03320a2c236df71c
diogo-h <diogo.m.henriques98@gmail.com>
2019-05-04 22:53:03 +0100

    Alterar e adicionar comentários na classe Player e Portal. Alterar lista da classe Player

--- programaSolução_InácioDiogoRafael/Player.cs
+++ programaSolução_InácioDiogoRafael/Player.cs
@@ -10,15 +10,11 @@ namespace programaSolução_InácioDiogoRafael
     /// </summary>
     class Player
     {
-        /// <summary>
-        /// Listas para o número de fantasmas que o jogador possui e para o 
-        /// número de jogadores que está na dungeon. Usamos uma lista em vez de
-        /// um array pois é possiver alterar o conteúdos da mesma 
-        /// dinamicamente.
-        /// </summary>
+        /* Usamos uma lista em vez de um array pois é possiver alterar o 
+         * conteúdos da mesma dinamicamente */
         // Lista do número de fantasmas que o jogador possui, começando com 9
-        List<string> playerGhosts = new List<string>(9);
+        List<int> playerGhosts = new List<int>(9);
         // Lista do número de fantasmas que estão na dungeon começando com 0
-        List<string> dungeonGhosts = new List<string>(0);
+        List<int> dungeonGhosts = new List<int>(0);
     }
 }
