23d09ca53429258babd3ca214b00f9f62528de52
diogo-h <diogo.m.henriques98@gmail.com>
2019-05-05 23:33:21 +0100

    Alterar Classe Portal

--- programaSolução_InácioDiogoRafael/Portal.cs
+++ programaSolução_InácioDiogoRafael/Portal.cs
@@ -9,5 +9,27 @@ namespace programaSolução_InácioDiogoRafael
     /// </summary>
     class Portal
     {
+        EnumPortalDirection direction;
+        // Rodar portal na direção dos ponteiros do relógio
+        public void ChangeDirection()
+        {
+            switch (direction)
+            {
+                case EnumPortalDirection.Left:
+                    direction = EnumPortalDirection.Up;
+                    break;
+                case EnumPortalDirection.Up:
+                    direction = EnumPortalDirection.Right;
+                    break;
+                case EnumPortalDirection.Right:
+                    direction = EnumPortalDirection.Down;
+                    break;
+                case EnumPortalDirection.Down:
+                    direction = EnumPortalDirection.Left;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
