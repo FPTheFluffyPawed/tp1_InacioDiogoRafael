3e176857d26cbac0199c3e1c6e400f703b5446fb
diogo-h <diogo.m.henriques98@gmail.com>
2019-05-05 23:24:30 +0100

    Adicionar EnumPortalDirection

---
+++ programaSolução_InácioDiogoRafael/EnumPortalDirection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace programaSolução_InácioDiogoRafael
+{
+    enum EnumPortalDirection
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+}
--- programaSolução_InácioDiogoRafael/Portal.cs
+++ programaSolução_InácioDiogoRafael/Portal.cs
@@ -9,11 +9,5 @@ namespace programaSolução_InácioDiogoRafael
     /// </summary>
     class Portal
     {
-        // Variables
-
-        // Sempre que um fantasma morrer, o portal roda 90graus clockwise
-
-        /* Fazer uma condição em que se um fantasma morrer, o portal da mesma 
-         * cor do fantasma que morreu roda */
     }
 }
