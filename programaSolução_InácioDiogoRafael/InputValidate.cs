816bc3ccb4f4895bb18d2060bb7adbd2700ed046
Rafael Castro e Silva <castroesilva.rafael@gmail.com>
2019-05-06 23:38:27 +0100

    Added static class that verifies if received Input is valid

---
+++ programaSolução_InácioDiogoRafael/InputValidate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace programaSolução_InácioDiogoRafael
+{   
+    /// <summary>
+    /// Capta os inputs dos jogadores.
+    /// </summary>
+    static class InputValidate
+    {
+        
+        static bool CheckSelectTile(string currentInput, Tile[,] tileArray)
+        {
+            string[] coords;
+            if (!currentInput.Contains(",")) return false;
+            else coords = currentInput.Split(",");
+
+            if (!Int32.TryParse(coords[0], out int x) || 
+                !Int32.TryParse(coords[1], out int y)) 
+            {
+                return false;
+            }
+            else 
+            {
+                if (tileArray[Int32.Parse(coords[0]),Int32.Parse(coords[1])] 
+                != null)
+                {
+                    return true;
+                }
+                else return false;
+            }
+        }
+
+        static bool CheckSelectGhost(string currentInput, Ghost[] ghostArray)
+        {
+            if(!Int32.TryParse(currentInput, out int g)) return false;
+
+
+        }
+
+
+    }
+}
