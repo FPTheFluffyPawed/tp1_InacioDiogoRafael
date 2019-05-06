bd18d7de747ec071d4ac7df9a8dc3852e6ed455a
FPTheFluffyPawed <n.ito10@hotmail.com>
2019-05-06 01:07:11 +0100

    pequeno fix para fazer testes

--- programaSolução_InácioDiogoRafael/Program.cs
+++ programaSolução_InácioDiogoRafael/Program.cs
@@ -9,23 +9,23 @@ namespace programaSolução_InácioDiogoRafael
         {
             // CODIGO TESTE
             // verificação se consige buscar informação do Tile
-            Tile tile = new Tile();
-            Console.WriteLine(tile.GetTileType(0, 0));
-            Console.WriteLine(tile.GetTileType(0, 1));
-            Console.WriteLine(tile.GetTileType(0, 2));
-            Console.WriteLine(tile.GetTileType(0, 3));
-            Console.WriteLine(tile.GetTileType(0, 4));
-            Console.WriteLine(tile.GetGhostType(0, 2));
+            Board board = new Board();
+            Console.WriteLine(board.GetTileType(0, 0));
+            Console.WriteLine(board.GetTileType(0, 1));
+            Console.WriteLine(board.GetTileType(0, 2));
+            Console.WriteLine(board.GetTileType(0, 3));
+            Console.WriteLine(board.GetTileType(0, 4));
 
             //parte complicada, testar introduzir fantasmas, não tem verificação mas podera ter no futuro em Player inputs
             // resumo: array utilizado para introduzir os valores, separar os valores, transformar em duas variaveis separadas e inserir para buscar
             // é IMPORTANTE que seja introduzido "<numero> <numero>" senão dá erro
             // é melhor ter a certeza com formas de verificar, claro!
-            string[] iGhost = Console.ReadLine().Split();
+
+            /*string[] iGhost = Console.ReadLine().Split();
             int insertedRow = int.Parse(iGhost[0]);
             int insertedColumn = int.Parse(iGhost[0]);
-            tile.SetGhostType(insertedRow, insertedColumn);
-            Console.WriteLine(tile.GetGhostType(insertedRow, insertedColumn));
+            board.SetGhostType(insertedRow, insertedColumn);
+            Console.WriteLine(board.GetGhostType(insertedRow, insertedColumn));*/
         }
     }
 }
