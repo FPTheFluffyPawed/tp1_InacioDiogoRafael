ba9156e264b716fa2deb20e9fb26d7a0e5996910
Rafael Castro e Silva <castroesilva.rafael@gmail.com>
2019-05-07 02:18:41 +0100

    added Position.cs, to help objects keep track of where they are on the grid without having to go trough the board arrays

---
+++ programaSolução_InácioDiogoRafael/Position.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace programaSolução_InácioDiogoRafael
+{
+    class Position
+    {
+        public int x {get; set;}
+        public int y {get; set;}
+
+        public Position(int initialX, int initialY)
+        {
+            x = initialX;
+            y = initialY;
+
+        }
+
+
+    }
+
+
+}
