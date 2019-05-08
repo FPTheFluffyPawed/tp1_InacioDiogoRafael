25d29c0f1094ae447cf634602d9bcb4a3c930cb5
Rafael Castro e Silva <castroesilva.rafael@gmail.com>
2019-05-08 21:26:43 +0100

    added class that stores messages that Renderer displays at certain times controlled by Game.cs

--- programaSolução_InácioDiogoRafael/PromptMessages.cs
+++ programaSolução_InácioDiogoRafael/PromptMessages.cs
@@ -1,7 +1,15 @@
 namespace programaSolução_InácioDiogoRafael
 {
-    public class EnumPromptMessages
+    static class PromptMessages
     {
-        
+        public static string  SelectGhost = "Escreve o número correspondente ao fantasma que queres selecionar, e depois carrega ENTER";
+
+        public static string SelectTile = "Escreve as coordenadas do quadrado onde queres colocar o fantasma selecionado, no formato 'x,y' sem as ''. e depois carrega ENTER";
+
+        public static string GhostFightWin = " ganhou a luta e enviou o adversário para a masmorra.";
+
+        public static string GhostFightLose = " perdeu a luta e foi enviado para a masmorra";
+
+
     }
 }
